53f4d3351cf2a5ec4b9b085b8adf54d22b9966ab
thu <thufelixc@gmail.com>
2018-03-20 17:03:59 +0800

    feat(ALL): 针对一个数据的绑定

--- src/main.js
+++ src/main.js
@@ -1,14 +1,40 @@
-// The Vue build version to load with the `import` command
-// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
-// import Vue from 'vue'
-// import App from './App'
+/**
+ * @file doris main
+ * @author thu
+ */
 
-// Vue.config.productionTip = false
+class Doris {
+    constructor(option) {
+        this.option = option;
+        this.el = document.querySelector(this.option.el);
+        this.data = {};
+        this.innerHTML = this.el.innerHTML;
 
-/* eslint-disable no-new */
-// new Vue({
-//   el: '#app',
-//   components: { App },
-//   template: '<App/>'
-// })
+        this.init();
 
+        /* setTimeout(() => {
+            this.data.name = 'thu';
+        }, 1000); */
+    }
+    init() {
+        for (let key of Object.keys(this.option.data)) {
+            Object.defineProperty(this.data, key, {
+                set: value => {
+                    this.refresh(key, value);
+                }
+            });
+        }
+        Object.assign(this.data, this.option.data);
+    }
+    refresh(key, value) {
+        this.el.innerHTML = this.innerHTML.replace(`{{ ${key} }}`, value);
+    }
+}
+
+new Doris({
+    el: '#app',
+    data: {
+        name: 'doris',
+        age: 22
+    }
+});
