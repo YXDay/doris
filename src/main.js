4dbda71b0339c9d85a5b756b43b0c43bfd4b9b80
thu <thufelixc@gmail.com>
2018-03-21 14:52:23 +0800

    feat(数据绑定): 数据绑定基础

--- src/main.js
+++ src/main.js
@@ -8,26 +8,34 @@ class Doris {
         this.option = option;
         this.el = document.querySelector(this.option.el);
         this.data = {};
+        this._data = {};
         this.innerHTML = this.el.innerHTML;
+
+        Object.assign(this._data, this.option.data);
 
         this.init();
 
-        /* setTimeout(() => {
-            this.data.name = 'thu';
-        }, 1000); */
+        // setTimeout(() => { this.data.name = 'thu'; }, 1000);
     }
     init() {
-        for (let key of Object.keys(this.option.data)) {
+        for (let key of Object.keys(this._data)) {
             Object.defineProperty(this.data, key, {
+                enumerable: true,
+                get: () => this._data[key],
                 set: value => {
-                    this.refresh(key, value);
+                    this._data[key] = value;
+                    this.refresh();
                 }
             });
         }
-        Object.assign(this.data, this.option.data);
+        Object.assign(this.data, this._data);
     }
-    refresh(key, value) {
-        this.el.innerHTML = this.innerHTML.replace(`{{ ${key} }}`, value);
+    refresh() {
+        let innerHTML = this.innerHTML;
+        for (let [key, value] of Object.entries(this.data)) {
+            innerHTML = innerHTML.replace(`{{ ${key} }}`, value);
+        }
+        this.el.innerHTML = innerHTML;
     }
 }
 
